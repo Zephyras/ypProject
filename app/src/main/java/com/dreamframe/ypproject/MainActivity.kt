c3a0bf544eaaaf1c57e97d42cd91ceb46c2dd8f7
hwang-kyowook <hkw0101@gmail.com>
2019-10-10 14:00:51 +0900

    Initial commit

---
+++ app/src/main/java/com/dreamframe/ypproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dreamframe.ypproject
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
